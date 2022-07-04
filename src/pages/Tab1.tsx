c219609fe3a23fa96575f73c92b34f8b1012da88
Sergey Popov <quolpr@gmail.com>
2022-07-04 10:06:43 +0300

    Init trong-orm for web

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,6 +1,12 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import { List } from "../components/List";
+import "./Tab1.css";
 
 const Tab1: React.FC = () => {
   return (
@@ -11,12 +17,9 @@ const Tab1: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <div className="content">
+          <List />
+        </div>
       </IonContent>
     </IonPage>
   );
